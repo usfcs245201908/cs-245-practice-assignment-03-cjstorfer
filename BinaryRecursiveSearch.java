8f0253d9c34fcead1f2b30555c373bfcfac22643
Christopher Storfer <cjstorfer@dons.usfca.edu>
2019-09-13 10:32:32 -0700

    fix of binary recursive search

--- BinaryRecursiveSearch.java
+++ BinaryRecursiveSearch.java
@@ -2,19 +2,20 @@ public class BinaryRecursiveSearch implements Practice03Search{
 	public String searchName(){
 		return "BinaryRecursiveSearch";
 	}
-	public int search(int [] arr, int target){
-		int low = 0;
-		int high = arr.length;
+	public int search(int [] arr, int target, int low, int high){
 		if (low>high)
 			return -1;
 		int mid = (high+low)/2;
 		if (arr[mid]==target)
 			return mid;
 		if (target>arr[mid])
-			return search(arr, target);
+			return search(arr, target, mid+1, high);
 		else
-			return search(arr, target);
+			return search(arr, target, low, mid-1);
 
+	}
+	public int search(int[] arr, int target){
+		return search(arr, target, 0, arr.length - 1);
 	}
 
 }
